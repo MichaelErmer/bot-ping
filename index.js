874019764cf355b0df8cf51e04f2e30e9702f8b3
Michael Ermer <michael.ermer@ermer.de>
2017-03-16 17:33:54 +0100

    bump

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 var _ = require('lodash');
+var ping = require ("net-ping");
+var isIPv6 = require('is-ipv6-node');
 
 var Module = function (bot) {
   this.bot = bot;
-  this.name = "Example Library";
+  this.name = "Ping Library";
   this.version = "0.1";
   this.help = function () {
     // RETURN HELP STRING FOR YOUR COMMANDS or AT LEAST YOUR COMMANDS Object.keys(this.commands)
     return {
-      "example": "Says hello.",
+      "ping": "Pings a host 3 times",
     };
   };
   this.commands = {};
 
-  this.commands.example = function(channel, args, user) {
-    var response = "Hello, "+user.name;
-    setTimeout(function() {
-      bot.postMessage(channel, response);
-    }, 500);
+  this.commands.ping = function(channel, args, user) {
+    var args = args.split(" ");
+    if (!args.length || !args[0]) bot.postMessage(channel, "Invalid Request");
+    var session = ping.createSession ();
+    
+    session.pingHost(args[0], function(error, target) {
+      if (error)
+        if (error instanceof ping.RequestTimedOutError)
+          return bot.postMessage(channel, target + " is not Alive.");
+        else
+          return bot.postMessage(channel, target + " Error: " + error.message);
+      else
+        return bot.postMessage(channel, target + " is Alive.");
+      session.close();
+    });
   };
 
 };
